5c69ce349f6688a710db330e5470a81b54594183
Sinedin <52830655+sinedindnd@users.noreply.github.com>
2021-05-20 16:17:58 -0500

    Add files via upload

---
+++ src/app/components/angular-material/angular-material.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import { MaterialModule } from '../../material.module';
+
+@Component({
+  selector: 'app-angular-material',
+  templateUrl: './angular-material.component.html',
+  styleUrls: ['./angular-material.component.css']
+})
+export class AngularMaterialComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
